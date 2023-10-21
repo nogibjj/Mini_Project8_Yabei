5c7e3074a3bff4025ae034af2a6df278be2042f7
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-10-21 17:58:40 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,11 @@
-use std::fs;
-use csv;
+use std::fs::File;
+use std::io::Write;
+use reqwest;
 
-// Data structure to hold the contents of the CSV file
-struct Data {
-    records: Vec<Vec<String>>,
-}
-
-impl Data {
-    // Load data from a CSV file
-    fn load_from_csv(file_path: &str) -> Result<Self, Box<dyn std::error::Error>> {
-        let mut rdr = csv::Reader::from_path(file_path)?;
-        let mut records = Vec::new();
-        for result in rdr.records() {
-            let record = result?;
-            records.push(record.iter().map(|s| s.to_string()).collect());
-        }
-        Ok(Self { records })
-    }
-
-    // Retrieve the top N records
-    fn get_top_n(&self, n: usize) -> Vec<Vec<String>> {
-        self.records.iter().take(n).cloned().collect()
-    }
-}
-
-fn extract() {
-    // Functionality to download data from a given URL and save it to a local CSV file
-}
-
-fn transform_and_load() -> Result<Data, Box<dyn std::error::Error>> {
-    // Load data from the CSV file and store it in memory
-    Data::load_from_csv("path_to_your_csv_file.csv")
-}
-
-fn general_query(data: &Data) {
-    // Simple query functionality to retrieve and display the top 10 records
-    let top_10_records = data.get_top_n(10);
-    for record in top_10_records {
-        println!("{:?}", record);
-    }
-}
-
-fn main() {
-    // Example: Load the data and execute a query
-    match transform_and_load() {
-        Ok(data) => {
-            general_query(&data);
-        }
-        Err(e) => {
-            println!("Error: {}", e);
-        }
-    }
+fn extract(url: &str, file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let response = reqwest::blocking::get(url)?;
+    let mut content = response.bytes()?;
+    let mut file = File::create(file_path)?;
+    file.write_all(&content)?;
+    Ok(())
 }
