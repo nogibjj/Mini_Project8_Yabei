f23d1ae151d2580d989eae748352a793adb5fc68
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-10-21 22:55:43 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,9 +1,14 @@
 import unittest
 from main import compute_statistics
 import pandas as pd
+import time
+import psutil
+
 
 
 class TestMainFunctions(unittest.TestCase):
+    start_time = time.time()
+
     @classmethod
     def setUpClass(cls):
         cls.df = pd.read_csv("cars.csv", sep=";")
@@ -14,6 +19,17 @@ class TestMainFunctions(unittest.TestCase):
         self.assertIn("median", stats)
         self.assertIn("std", stats)
         self.assertIn("size", stats)
+        end_time = time.time()
+       
+        elapsed_time = end_time - start_time
+
+        cpu_percent = psutil.cpu_percent()
+        memory_info = psutil.virtual_memory()
+
+        print(f"Elapsed time: {elapsed_time:.4f} seconds")
+        print(f"CPU Usage: {cpu_percent}%")
+        print(f"Memory Usage: {memory_info.percent}%")
+
 
 
 if __name__ == "__main__":
