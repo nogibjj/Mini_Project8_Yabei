b2552f45a15366125ac3e869df6c5258fafcef4b
GitHub Action <action@github.com>
2023-10-22 01:47:40 +0000

    Add metric log

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from mylib.lib import compute_statistics
 import pandas as pd
 
+
 def main():
     # Load the dataset
-    df = pd.read_csv("cars.csv", sep=';')
-    
-    column_name = 'Weight'
-    
+    df = pd.read_csv("cars.csv", sep=";")
+
+    column_name = "Weight"
+
     if column_name in df.columns:
         stats = compute_statistics(df, column_name)
-        
+
         # Print the computed statistics
         print(f"Statistics for column '{column_name}':")
         print(f"Mean: {stats['mean']:.2f}")
@@ -20,7 +21,5 @@ def main():
         print(f"Column '{column_name}' not found in the dataset.")
 
 
-if __name__ == "__main__":  
+if __name__ == "__main__":
     main()
-
-    
--- test_main.py
+++ test_main.py
@@ -2,18 +2,19 @@ import unittest
 from main import compute_statistics
 import pandas as pd
 
-class TestMainFunctions(unittest.TestCase):
 
+class TestMainFunctions(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.df = pd.read_csv("cars.csv", sep=';')
+        cls.df = pd.read_csv("cars.csv", sep=";")
 
     def test_compute_statistics(self):
-        stats = compute_statistics(self.df, 'Weight')
-        self.assertIn('mean', stats)
-        self.assertIn('median', stats)
-        self.assertIn('std', stats)
-        self.assertIn('size', stats)
+        stats = compute_statistics(self.df, "Weight")
+        self.assertIn("mean", stats)
+        self.assertIn("median", stats)
+        self.assertIn("std", stats)
+        self.assertIn("size", stats)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
