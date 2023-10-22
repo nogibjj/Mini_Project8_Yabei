15358b8dd147e8a800a0a24b806cb56d2e8c101c
yabeizeng1121 <143656459+yabeizeng1121@users.noreply.github.com>
2023-10-21 22:13:39 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-// Removed the redundant import
 use std::fs::File;
 use std::io::Write;
+use std::time::Instant;
 
 fn extract(url: &str, file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
     let response = reqwest::blocking::get(url)?;
-    // Removed the mut keyword
     let content = response.bytes()?;
     let mut file = File::create(file_path)?;
     file.write_all(&content)?;
@@ -12,6 +11,8 @@ fn extract(url: &str, file_path: &str) -> Result<(), Box<dyn std::error::Error>>
 }
 
 fn main() {
+    let start_time = Instant::now();
+
     let url = "https://github.com/nogibjj/Mini_Project5_Yabei_New/blob/main/cars.csv?raw=true";
     let file_path = "output.txt";
 
@@ -22,4 +23,7 @@ fn main() {
         ),
         Err(e) => eprintln!("Error occurred: {}", e),
     }
+
+    let duration = start_time.elapsed();
+    println!("Execution time: {:?}", duration);
 }
